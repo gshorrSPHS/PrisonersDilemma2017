6a410cb7fc5808761ab87ac585cf6f1a3c771bb7
LimBui <limxbui@gmail.com>
2017-03-07 10:26:46 -0800

    Update and rename team0.py to team12.py
    Updated team info.

--- team12.py
+++ team12.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Pho Dac Biet' # Only 10 chars displayed.
+strategy_name = 'Above or Below?'
+strategy_description = 'It decides to collude or betray based on the percentage of the amount of the other teams betrays?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
