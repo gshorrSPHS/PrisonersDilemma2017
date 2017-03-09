cda700b34078d4cfe7911d31b7b38c6d011c27d9
GarrettShorr <g@garrettshorr.com>
2017-03-09 09:34:32 -0800

    Update team17.py

--- team17.py
+++ team17.py
@@ -26,7 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
