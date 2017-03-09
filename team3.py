b2335d0d2dd1227a10787fc807d996a955da16aa
AWang <KHLover1234@gmail.com>
2017-03-09 09:34:05 -0800

    Put in working code for the prisoner's dilemma

--- team3.py
+++ team3.py
@@ -6,9 +6,12 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+import numpy
+from numpy import random
+
+team_name = '_Gol_den_' # Only 10 chars displayed.
+strategy_name = 'Random'
+strategy_description = 'Each set of rounds has a different percent chance of a collude'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,10 +28,118 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    # Colllude in the first 3 rounds, betray the rest
+    if len(my_history) < 3:
+        if random.uniform() < 0.5:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 8:
+        if random.uniform() < 0.9:
+            return 'b'
+        else:
+            return 'c'
+    elif len(my_history) < 13:
+        if random.uniform() < 0.6:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 20:
+        if random.uniform() <0.2:
+            return 'c'
+        else: 
+            return 'b'
+    elif len(my_history) < 25:
+        if random.uniform() < 0.7:
+            return 'b'
+        else:
+            return 'c'
+    elif len(my_history) < 33:
+        if random.uniform() <0.9:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 35:
+        return 'b'
+    elif len(my_history) < 40:
+        if random.uniform() <0.5:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 50:
+        if random.uniform() < 0.6:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 56:
+        if random. uniform() < 0.7:
+            return 'b'
+        else:
+            return 'c'
+    elif len(my_history) < 60:
+        if random.uniform() <0.4:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 63:
+        if random.uniform() <0.9:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 68:
+        if random.uniform() <0.5:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 72:
+        if random.uniform() < 0.2:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 78:
+        if random.uniform() < 0.1:
+            return 'b'
+        else:
+            return 'c'
+    elif len(my_history) < 83:
+        if random.uniform() < 0.8:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 87:
+        if random.uniform() < 0.7:
+            return 'b'
+        else: 
+            return 'c'
+    elif len(my_history) < 90:
+        if random.uniform() < 0.9:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 94:
+        if random.uniform() < 0.6:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) < 97:
+        if random.uniform() < 0.4:
+            return 'c'
+        else:
+            return 'b'
+    elif len(my_history) <101:
+        if random.uniform() < 0.2:
+            return 'c'
+        else:
+            return 'b'
+    elif random.uniform() < 0.5:
+        return 'c'
+    else:
+        return 'b'
 
     
+    
+    
+        
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -48,8 +159,8 @@ def test_move(my_history, their_history, my_score, their_score, result):
 if __name__ == '__main__':
      
     # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
+    if test_move(my_history='cbc',
+              their_history='bcb', 
               my_score=0,
               their_score=0,
               result='b'):
