b097d917c8c2bda010f35180d3a72efbaa25e729
GarrettShorr <g@garrettshorr.com>
2017-03-13 08:09:38 -0700

    Update team17.py

--- team17.py
+++ team17.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Always Frontstab' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
