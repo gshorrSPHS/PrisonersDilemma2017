58d645e9c736908dfd69bacdb67237018a7ec7e3
ColeChuang <colechuang@yahoo.com>
2017-03-13 08:29:18 -0700

    added len()

--- Team7.py
+++ Team7.py
@@ -4,7 +4,7 @@ strategy_name = 'monkey see monkey do'
 strategy_description = 'starts with betray then does what opponent did last turn'
     
 def move(my_history, their_history, my_score, their_score):
-    if my_history == []:
+    if len(my_history) == 0:
         return 'b'
     else:
         return their_history[-1]
