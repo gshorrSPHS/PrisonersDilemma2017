f203d07e67af42948e6d5b9eaef40853be6d225c
JustinCCabezas <justincabezas@gmail.com>
2017-03-13 09:02:06 -0700

    Update Team0.py

--- Team0.py
+++ Team0.py
@@ -7,9 +7,9 @@
 ####
 import random 
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'X-Force' # Only 10 chars displayed.
+strategy_name = 'Random'
+strategy_description = 'Picks b or c randomly'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
