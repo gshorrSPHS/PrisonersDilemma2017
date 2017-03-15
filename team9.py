6f6e06a55cf9ec83a20eef65a8bb45d4e74eca0e
azhou2 <azhou2@users.noreply.github.com>
2017-03-15 08:42:43 -0700

    Fixed various bugs that returned blanks.

--- team9.py
+++ team9.py
@@ -14,17 +14,21 @@ strategy_description = 'Our strategy is none of your business, but if you must k
 from numpy import random
 
 def move(my_history, their_history, my_score, their_score):
-    choices = ["b", "c"]
+    choices = "bc"
     if random.uniform() < 0.1:
         return random.choice(choices)
-    if 1 <= len(my_history) + 1 and len(my_history) <= 10: #rounds 1-10 will be random
-        return random.choice(choices)
-    elif 11 <= len(my_history) + 1 and len(my_history) <= 20: #if they alternate, return b
+    if 1 <= len(my_history) + 1 and len(my_history) + 1 <= 10: #rounds 1-10 will be random
+        return random.choice("b","c")
+    elif 11 <= len(my_history) + 1 and len(my_history) + 1 <= 20: #if they alternate, return b
         if "cbcb" in their_history:
             return "b"
+        elif "bbbbb" in their_history:
+            return "b"
+        elif "ccccc" in their_history:
+            return "c"
         else:
             return "c"
-    elif 21 <= len(my_history) + 1 and len(my_history) <= 30: #If they betray > 50%, betray
+    elif 21 <= len(my_history) + 1 and len(my_history) + 1 <= 30: #If they betray > 50%, betray
         countB = 0
         for letter in their_history:
 	   if letter == "b":
@@ -33,12 +37,12 @@ def move(my_history, their_history, my_score, their_score):
 	    return "b"
 	else:
 	    return "c"
-    elif 31 <= len(my_history) + 1 and len(my_history) <= 40: #if they betray, random. else, betray
+    elif 31 <= len(my_history) + 1 and len(my_history) + 1 <= 40: #if they betray, random. else, betray
         if their_history[len(their_history)] == "b":
             return random.choice(choices)
         else:
             return "b"
-    elif 41 <= len(my_history) + 1 and len(my_history) <= 50: #If collude > 50%, betray
+    elif 41 <= len(my_history) + 1 and len(my_history) + 1 <= 50: #If collude > 50%, betray
         countC = 0
         for letter in their_history:
 	   if letter == "c":
@@ -47,26 +51,26 @@ def move(my_history, their_history, my_score, their_score):
 	    return "b"
 	else:
 	    return "c"
-    elif 51 <= len(my_history) + 1 and len(my_history) <= 60: #if "bbb" in their_history, betray
+    elif 51 <= len(my_history) + 1 and len(my_history) + 1 <= 60: #if "bbb" in their_history, betray
         if "bbb" in their_history:
             return "b"
         else:
             return "c"
-    elif 61 <= len(my_history) + 1 and len(my_history) <= 70: #if "ccc" in their_history, betray
+    elif 61 <= len(my_history) + 1 and len(my_history) + 1 <= 70: #if "ccc" in their_history, betray
         if "ccc" in their_history:
             return "b"
         else:
             return "c"
-    elif 71 <= len(my_history) + 1 and len(my_history) <= 80: #return their thingy from 5 rounds back
+    elif 71 <= len(my_history) + 1 and len(my_history) + 1 <= 80: #return their thingy from 5 rounds back
         return their_history[-4]
-    elif 81 <= len(my_history) + 1 and len(my_history) <= 90: #alternate, start on c
+    elif 81 <= len(my_history) + 1 and len(my_history) + 1 <= 90: #alternate, start on c
         if (len(my_history) + 1) % 2 == 0:
             return "b"
         else:
             return "c"
-    elif 91 <= len(my_history) + 1 and len(my_history) <= 100: #always betray
+    elif 91 <= len(my_history) + 1 and len(my_history) + 1 <= 100: #always betray
         return "b"
-    elif 101 <= len(my_history) + 1 and len(my_history) <= 110: #"bcbbcccbbb"
+    elif 101 <= len(my_history) + 1 and len(my_history) + 1 <= 110: #"bcbbcccbbb"
         if len(my_history) + 1 == 101:
             return "b"
         elif len(my_history) + 1 == 102:
@@ -90,12 +94,12 @@ def move(my_history, their_history, my_score, their_score):
         else:
             print "Error in 101-110 range."
             
-    elif 111 <= len(my_history) + 1 and len(my_history) <= 120: #if round number is divisible by 3, do c
+    elif 111 <= len(my_history) + 1 and len(my_history) + 1 <= 120: #if round number is divisible by 3, do c
         if (len(my_history) + 1) % 3 == 0:
             return "b"
         else:
             return "c"
-    elif 121 <= len(my_history) + 1 and len(my_history) <= 130: #bccbbccbbc
+    elif 121 <= len(my_history) + 1 and len(my_history) + 1 <= 130: #bccbbccbbc
         if len(my_history) + 1 == 121:
             return "b"
         elif len(my_history) + 1 == 122:
@@ -118,7 +122,7 @@ def move(my_history, their_history, my_score, their_score):
             return "c"
         else:
             print "Error in 121-130 range."
-    elif 131 <= len(my_history) + 1 and len(my_history) <= 140: #bbcbcbbbbc
+    elif 131 <= len(my_history) + 1 and len(my_history) + 1 <= 140: #bbcbcbbbbc
         if len(my_history) + 1 == 131:
             return "b"
         elif len(my_history) + 1 == 132:
@@ -141,12 +145,12 @@ def move(my_history, their_history, my_score, their_score):
             return "c"
         else:
             print "Error in 131-140 range."
-    elif 141 <= len(my_history) + 1 and len(my_history) <= 150: #alternate, start on b
+    elif 141 <= len(my_history) + 1 and len(my_history) + 1 <= 150: #alternate, start on b
         if (len(my_history) + 1) % 2 == 0:
             return "c"
         else:
             return "b"
-    elif 151 <= len(my_history) + 1 and len(my_history) <= 160: #bccbbbcccc
+    elif 151 <= len(my_history) + 1 and len(my_history) + 1 <= 160: #bccbbbcccc
         if len(my_history) + 1 == 151:
             return "b"
         elif len(my_history) + 1 == 152:
@@ -169,7 +173,7 @@ def move(my_history, their_history, my_score, their_score):
             return "c" 
         else:
             print "Error in 151-160 range."
-    elif 161 <= len(my_history) + 1 and len(my_history) <= 170: #cbbccbbccb
+    elif 161 <= len(my_history) + 1 and len(my_history) + 1 <= 170: #cbbccbbccb
         if len(my_history) + 1 == 161:
             return "c"
         elif len(my_history) + 1 == 162:
@@ -192,7 +196,7 @@ def move(my_history, their_history, my_score, their_score):
             return "b"
         else:
             print "Error in 161-170 range."
-    elif 171 <= len(my_history) + 1 and len(my_history) <= 180: #cbbcccbbbb
+    elif 171 <= len(my_history) + 1 and len(my_history) + 1 <= 180: #cbbcccbbbb
         if len(my_history) + 1 == 171:
             return "c"
         elif len(my_history) + 1 == 172:
@@ -215,12 +219,12 @@ def move(my_history, their_history, my_score, their_score):
             return "b" 
         else:
             print "Error in 171-180 range."
-    elif 181 <= len(my_history) + 1 and len(my_history) <= 190: #if round number is divisible by 3, do b
+    elif 181 <= len(my_history) + 1 and len(my_history) + 1 <= 190: #if round number is divisible by 3, do b
         if (len(my_history) + 1) % 3 == 0:
             return "c"
         else:
             return "b"
-    elif 191 <= len(my_history) + 1 and len(my_history) <= 200: #always betray
+    elif 191 <= len(my_history) + 1 and len(my_history) + 1 <= 200: #always betray
         return "b"
     else:
         print "Error. Round number does not match programmed ranges."
